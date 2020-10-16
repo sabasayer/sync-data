87b6db2ca2abc90dc69c1becaae769c60b47d04a
salihbaki <salihbakisayer@hotmail.com.tr>
2020-10-16 13:58:57 +0300

    effect condition

--- src/sync-master/sync-master.spec.ts
+++ src/sync-master/sync-master.spec.ts
@@ -1,133 +1,155 @@
 import { SyncMaster } from "./sync-master";
 import { Syncable } from "../syncable/syncable";
 import { EnumEffect } from "./effect.enum";
-import {effectsSync,effects} from "../decorators/effects.decorator"
+import { effectsSync, effects } from "../decorators/effects.decorator";
 
 describe("Sync Master", () => {
-    class Test1 extends Syncable<number> {
-        items: number[] = [1, 2];
+  class Test1 extends Syncable<number> {
+    items: number[] = [1, 2];
 
-        constructor() {
-            super("test");
-        }
-
-        async get() {
-            this.items = [1, 2];
-        }
-
-        add(item: number) {
-            this.items.push(item);
-        }
-
-        remove(item: number) {
-            this.items.splice(this.items.indexOf(item), 1);
-        }
-
-        syncCondition(item: number) {
-            return item > 150;
-        }
+    constructor() {
+      super("test");
     }
 
-    class Test2 extends Syncable<number> {
-        items: number[] = [1, 2, 3, 5];
-
-        constructor() {
-            super("test2");
-        }
-
-        async get() {
-            this.items = [1, 2, 3, 5, 6, 7];
-        }
-
-        add(item: number) {
-            this.items.push(item);
-        }
-
-        syncCondition(item: number) {
-            return item < 150;
-        }
+    async get() {
+      this.items = [1, 2];
     }
 
-    class EffectTestClass {
-       @effects("test",{effect:EnumEffect.Added})
-       static async runAddedEffect(value:number){
-            return new Promise((resolve)=>{
-                setTimeout(()=>{
-                    resolve(value);
-                },1000)
-            })
-        }
-
-        @effectsSync("test",{effect:EnumEffect.Added})
-        static runAddedEffectSync(value:number){
-            return value
-        }
+    add(item: number) {
+      this.items.push(item);
     }
 
-    test("should add new items to test1", () => {
-        let instance1 = new Test1();
-        let instance2 = new Test1();
+    remove(item: number) {
+      this.items.splice(this.items.indexOf(item), 1);
+    }
 
-        SyncMaster.effect("test", { effect: EnumEffect.Added, data: 250 });
+    syncCondition(item: number) {
+      return item > 150;
+    }
+  }
 
-        expect(instance1.items).toEqual([1, 2, 250]);
-        expect(instance2.items).toEqual([1, 2, 250]);
+  class Test2 extends Syncable<number> {
+    items: number[] = [1, 2, 3, 5];
+
+    constructor() {
+      super("test2");
+    }
+
+    async get() {
+      this.items = [1, 2, 3, 5, 6, 7];
+    }
+
+    add(item: number) {
+      this.items.push(item);
+    }
+
+    syncCondition(item: number) {
+      return item < 150;
+    }
+  }
+
+  class EffectTestClass {
+    @effects("test", { effect: EnumEffect.Added })
+    static async runAddedEffect(value: number) {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(value);
+        }, 1000);
+      });
+    }
+
+    @effectsSync("test", { effect: EnumEffect.Added })
+    static runAddedEffectSync(value: number) {
+      return value;
+    }
+
+    @effectsSync("test", {
+      effect: EnumEffect.Added,
+      condition: (data: number) => data > 20,
+    })
+    static runAddWithCondition(value: number) {
+      return value;
+    }
+  }
+
+  test("should add new items to test1", () => {
+    let instance1 = new Test1();
+    let instance2 = new Test1();
+
+    SyncMaster.effect("test", { effect: EnumEffect.Added, data: 250 });
+
+    expect(instance1.items).toEqual([1, 2, 250]);
+    expect(instance2.items).toEqual([1, 2, 250]);
+  });
+
+  test("syncConditions should work", () => {
+    let instance1 = new Test1();
+    let instance2 = new Test2();
+
+    SyncMaster.effect("test", { effect: EnumEffect.Added, data: 149 });
+    SyncMaster.effect("test2", { effect: EnumEffect.Added, data: 151 });
+
+    expect(instance1.items).toEqual([1, 2]);
+    expect(instance2.items).toEqual([1, 2, 3, 5]);
+  });
+
+  test("remove should work", () => {
+    let instance1 = new Test1();
+
+    SyncMaster.effect("test", { effect: EnumEffect.Removed, data: 1 });
+
+    expect(instance1.items).toEqual([2]);
+  });
+
+  test("shide effects should work", () => {
+    let instance1 = new Test1();
+    let instance2 = new Test2();
+
+    SyncMaster.effect("test", {
+      effect: EnumEffect.Added,
+      data: 122,
+      sideEffectedKeys: ["test2"],
     });
 
-    test("syncConditions should work",()=>{
-        let instance1 = new Test1();
-        let instance2 = new Test2();
+    expect(instance2.items).toEqual([1, 2, 3, 5, 6, 7]);
+  });
 
-        SyncMaster.effect("test", { effect: EnumEffect.Added, data: 149 });
-        SyncMaster.effect("test2", { effect: EnumEffect.Added, data: 151 });
+  test("shide effects should work when there isnt any registered main syncable", () => {
+    SyncMaster.clear();
 
-        expect(instance1.items).toEqual([1, 2]);
-        expect(instance2.items).toEqual([1, 2, 3, 5]);
+    let instance2 = new Test2();
+
+    SyncMaster.effect("test", {
+      effect: EnumEffect.Added,
+      data: 122,
+      sideEffectedKeys: ["test2"],
     });
 
-    test("remove should work",()=>{
-        let instance1 = new Test1();
+    expect(instance2.items).toEqual([1, 2, 3, 5, 6, 7]);
+  });
 
-        SyncMaster.effect("test",{effect:EnumEffect.Removed,data:1});
+  test("unregister should work", () => {
+    let instance1 = new Test1();
 
-        expect(instance1.items).toEqual([2]);
-    });
+    SyncMaster.unregister("test", instance1);
+    SyncMaster.effect("test", { effect: EnumEffect.Added, data: 250 });
+    expect(instance1.items).toEqual([1, 2]);
+  });
 
-    test("shide effects should work",()=>{
-        let instance1 = new Test1();
-        let instance2 = new Test2();
+  test("decorator should work for sync and async methods", async () => {
+    let instance1 = new Test1();
+    let res1 = await EffectTestClass.runAddedEffect(351);
+    let res2 = EffectTestClass.runAddedEffectSync(352);
 
-        SyncMaster.effect("test",{effect:EnumEffect.Added,data:122,sideEffectedKeys:["test2"]});
+    expect(instance1.items).toEqual([1, 2, 351, 352]);
+    expect(res1).toEqual(351);
+    expect(res2).toEqual(352);
+  });
 
-        expect(instance2.items).toEqual([1, 2, 3, 5, 6, 7])
-    })
+  test("should check for condition", () => {
+    let instance1 = new Test1();
+    let res2 = EffectTestClass.runAddWithCondition(13);
 
-    test("shide effects should work when there isnt any registered main syncable",()=>{
-        SyncMaster.clear();
-
-        let instance2 = new Test2();
-        
-        SyncMaster.effect("test",{effect:EnumEffect.Added,data:122,sideEffectedKeys:["test2"]});
-
-        expect(instance2.items).toEqual([1, 2, 3, 5, 6, 7])
-    })
-
-    test("unregister should work",()=>{
-        let instance1 = new Test1();
-
-        SyncMaster.unregister("test",instance1);
-        SyncMaster.effect("test", { effect: EnumEffect.Added, data: 250 });
-        expect(instance1.items).toEqual([1, 2]);
-    })
-
-
-    test("decorator should work for sync and async methods",async ()=>{
-        let instance1 = new Test1();
-        let res1 = await EffectTestClass.runAddedEffect(351);
-        let res2 = EffectTestClass.runAddedEffectSync(352);
-
-        expect(instance1.items).toEqual([1,2,351,352])
-        expect(res1).toEqual(351)
-        expect(res2).toEqual(352)
-    })
+    expect(instance1.items).toEqual([1, 2]);
+  });
 });
--- src/sync-master/sync-master.ts
+++ src/sync-master/sync-master.ts
@@ -2,96 +2,104 @@ import { ISyncable } from "../syncable/syncable.interface";
 import { EnumEffect } from "./effect.enum";
 
 export interface SyncMasterEffectOptions<T> {
-    effect?: EnumEffect;
-    data?: T;
-    sideEffectedKeys?: string[];
+  effect?: EnumEffect;
+  data?: T;
+  sideEffectedKeys?: string[];
+  condition?: (data: T) => boolean;
 }
 
 export abstract class SyncMaster {
-    private static store: { [key: string]: ISyncable<any>[] } = {};
+  private static store: { [key: string]: ISyncable<any>[] } = {};
 
-    static register<T>(key: string, owner: ISyncable<T>) {
-        if (!this.store[key]) {
-            this.store[key] = [];
-        }
-
-        this.store[key].push(owner);
+  static register<T>(key: string, owner: ISyncable<T>) {
+    if (!this.store[key]) {
+      this.store[key] = [];
     }
 
-    static unregister<T>(key: string, owner: ISyncable<T>) {
-        if (!this.store[key]) return;
+    this.store[key].push(owner);
+  }
 
-        let index = this.store[key].indexOf(owner);
-        if (index > -1) this.store[key].splice(index, 1);
+  static unregister<T>(key: string, owner: ISyncable<T>) {
+    if (!this.store[key]) return;
+
+    let index = this.store[key].indexOf(owner);
+    if (index > -1) this.store[key].splice(index, 1);
+  }
+
+  static checkCondition<T>(options?: SyncMasterEffectOptions<T>) {
+    return (
+      !options?.condition || !options?.data || options.condition(options.data)
+    );
+  }
+
+  static effect<T>(key: string, options?: SyncMasterEffectOptions<T>) {
+    if (!SyncMaster.checkCondition(options)) {
+      return;
     }
 
-    static effect<T>(
-        key: string,
-        options?: SyncMasterEffectOptions<T>
-    ) {
-        let syncables = this.store[key];
+    let syncables = this.store[key];
 
-        if (syncables?.length) {
-            switch (options?.effect) {
-                case EnumEffect.Added:
-                    syncables.forEach((syncable: ISyncable<T>) => {
-                        SyncMaster.onAdded(syncable, options.data);
-                    });
-                    break;
-                case EnumEffect.Updated:
-                    syncables.forEach((syncable: ISyncable<T>) => {
-                        SyncMaster.onUpdated(syncable, options.data);
-                    });
-                    break;
-                case EnumEffect.Removed:
-                    syncables.forEach((syncable: ISyncable<T>) => {
-                        SyncMaster.onRemoved(syncable, options.data);
-                    });
-                    break;
-                default:
-                    syncables.forEach((syncable: ISyncable<T>) => {
-                        SyncMaster.onSync(syncable, options?.data);
-                    });
-                    break;
-            }
-        }
-
-        options?.sideEffectedKeys?.forEach(sideEffectedKey => {
-            SyncMaster.effect(sideEffectedKey, { effect: options.effect });
-        });
+    if (syncables?.length) {
+      switch (options?.effect) {
+        case EnumEffect.Added:
+          syncables.forEach((syncable: ISyncable<T>) => {
+            SyncMaster.onAdded(syncable, options.data);
+          });
+          break;
+        case EnumEffect.Updated:
+          syncables.forEach((syncable: ISyncable<T>) => {
+            SyncMaster.onUpdated(syncable, options.data);
+          });
+          break;
+        case EnumEffect.Removed:
+          syncables.forEach((syncable: ISyncable<T>) => {
+            SyncMaster.onRemoved(syncable, options.data);
+          });
+          break;
+        default:
+          syncables.forEach((syncable: ISyncable<T>) => {
+            SyncMaster.onSync(syncable, options?.data);
+          });
+          break;
+      }
     }
 
-    static clear() {
-        this.store = {};
-    }
+    options?.sideEffectedKeys?.forEach((sideEffectedKey) => {
+      SyncMaster.effect(sideEffectedKey, { effect: options.effect });
+    });
+  }
 
-    private static checkSyncable<T>(syncable: ISyncable<T>, data?: T) {
-        return (
-            !syncable.syncCondition ||
-            !data ||
-            syncable.syncCondition?.call(syncable, data)
-        );
-    }
+  static clear() {
+    this.store = {};
+  }
 
-    private static onAdded<T>(syncable: ISyncable<T>, data?: T) {
-        if (!SyncMaster.checkSyncable(syncable, data)) return;
+  private static checkSyncable<T>(syncable: ISyncable<T>, data?: T) {
+    return (
+      !syncable.syncCondition ||
+      !data ||
+      syncable.syncCondition?.call(syncable, data)
+    );
+  }
 
-        if (syncable.add && data) syncable.add?.call(syncable, data);
-        else syncable.get.call(syncable);
-    }
+  private static onAdded<T>(syncable: ISyncable<T>, data?: T) {
+    if (!SyncMaster.checkSyncable(syncable, data)) return;
 
-    private static onUpdated<T>(syncable: ISyncable<T>, data?: T) {
-        if (syncable.update && data) syncable.update?.call(syncable, data);
-        else syncable.get.call(syncable);
-    }
+    if (syncable.add && data) syncable.add?.call(syncable, data);
+    else syncable.get.call(syncable);
+  }
 
-    private static onRemoved<T>(syncable: ISyncable<T>, data?: T) {
-        if (syncable.remove && data) syncable.remove?.call(syncable, data);
-        else syncable.get.call(syncable);
-    }
+  private static onUpdated<T>(syncable: ISyncable<T>, data?: T) {
+    if (syncable.update && data) syncable.update?.call(syncable, data);
+    else syncable.get.call(syncable);
+  }
 
-    private static onSync<T>(syncable: ISyncable<T>, data?: T) {
-        if (!SyncMaster.checkSyncable(syncable, data)) return;
-        syncable.get.call(syncable);
-    }
+  private static onRemoved<T>(syncable: ISyncable<T>, data?: T) {
+    if (syncable.remove && data) syncable.remove?.call(syncable, data);
+    else syncable.get.call(syncable);
+  }
+
+  private static onSync<T>(syncable: ISyncable<T>, data?: T) {
+    if (!SyncMaster.checkSyncable(syncable, data)) return;
+    syncable.get.call(syncable);
+  }
 }
